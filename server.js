6c696b255bc33d6f25b74ce394b8aaad1ee97230
GerailtonNeves <gerailton.neves@hotmail.com>
2026-08-23 12:26:52 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -32,6 +32,14 @@ app.use(cors());
 app.use(express.json({ limit: '15mb' }));
 app.use(express.urlencoded({ limit: '15mb', extended: true }));
 
+// Desativar cache do navegador para garantir carregamento limpo e instantâneo das rotas
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public'), { index: 'index.html' }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
