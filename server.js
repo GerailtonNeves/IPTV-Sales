18e69e4e3b32cbb9e3b181fc2fdf6b35e34f990f
GerailtonNeves <gerailton.neves@hotmail.com>
2026-08-23 11:17:09 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -36,9 +36,26 @@ app.use(express.static(path.join(__dirname, 'public'), { index: 'index.html' }))
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 let fallbackDashboardHtml = '';
-try {
-  fallbackDashboardHtml = require('./dashboard_html');
-} catch (e) {}
+let jsAppBundle = '';
+let cssStyleBundle = '';
+try { fallbackDashboardHtml = require('./dashboard_html'); } catch (e) {}
+try { jsAppBundle = require('./js_app_bundle'); } catch (e) {}
+try { cssStyleBundle = require('./css_style_bundle'); } catch (e) {}
+
+// Servir os arquivos de script e CSS com MIME types corretos no Render
+app.get(['/js/app.js', '/public/js/app.js'], (req, res) => {
+  const p1 = path.resolve(__dirname, 'public', 'js', 'app.js');
+  if (fs.existsSync(p1)) return res.type('application/javascript').sendFile(p1);
+  if (jsAppBundle) return res.type('application/javascript').send(jsAppBundle);
+  res.status(404).send('// JS not found');
+});
+
+app.get(['/css/style.css', '/public/css/style.css'], (req, res) => {
+  const p1 = path.resolve(__dirname, 'public', 'css', 'style.css');
+  if (fs.existsSync(p1)) return res.type('text/css').sendFile(p1);
+  if (cssStyleBundle) return res.type('text/css').send(cssStyleBundle);
+  res.status(404).send('/* CSS not found */');
+});
 
 // Servir o painel web index.html na rota principal (Garante carregamento completo do HTML no Render)
 app.get(['/', '/index.html'], (req, res) => {
